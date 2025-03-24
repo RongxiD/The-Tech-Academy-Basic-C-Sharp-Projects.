9a3c008b1a8456b34cb4d218ddd18ae09127615e
RongxiD <drxxdu@gmail.com>
2025-03-23 19:58:55 -0700

    homework

---
+++ Writing and Running Code/AcmeAccounting.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class AcmeAccounting
+{
+    public static void Main(string[] args)
+    {
+        // 1. Print the first message to the console.
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+
+        // 2. Print the second message to the console on a new line.
+        Console.WriteLine("Remember, we're \"accounting\" on you!"); 
+    }
+}
